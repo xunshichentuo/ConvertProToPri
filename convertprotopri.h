a9bbe3fda7f0307fb87e2028b620e79e754d797a
chentuo <chentuo@soonsolid.com>
2020-11-06 15:41:34 +0800

    refactoring:deleted convertprotopri class old code.

--- convertprotopri.h
+++ convertprotopri.h
@@ -11,25 +11,6 @@ class ConvertProToPri : public QObject
 public:
     explicit ConvertProToPri(QObject *parent = nullptr);
     explicit ConvertProToPri(const QString srcFilePath, QObject *parent = nullptr);
-
-    QString convertFile();
-    QString convertNextLine();
-    QString convertOneLine(const int index);
-    bool needConvert(const int index);
-    QString readOneLineFromSourceFile(const QString srcPath);
-    int getSourceFileLineNumber(const QString srcPath);
-
-private:
-    void loadSrcFile(const QString srcFilePath);
-    bool isOpenSrcFile();
-    bool isValidIndex(const int index);
-    QString convertPwdPath(const QString &lineContent);
-private:
-    void moveToFileHead();
-    void moveToSpecifiedLine(const int index);
-    QString getSpecifiedLineContent(const int index);
-    int getFirstKeyWordLineNumber(const QString filePath, const QString keyword);
-    QString getFirstKeyWordLineData(const QString filePath, const QString keyword);
 private:
     QString proFilePath;
     QFile srcFile;
