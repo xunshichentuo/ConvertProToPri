4f38e3a944a5794938cb8cd2639bc69a5c27aab5
chentuo <chentuo@soonsolid.com>
2020-11-09 10:15:07 +0800

    clean up and refactoring

--- ProToPriTool/main.cpp
+++ ProToPriTool/main.cpp
@@ -1,52 +1,60 @@
 #include <QCoreApplication>
 #include <QFile>
 #include <QDebug>
+#include <QFileInfo>
 #include "profilereader.h"
 #include "parseproconfig.h"
+
+void printUsage(const QString &fileName)
+{
+    qDebug()<<QString("usage: %1 [src path] [dst path]").arg(fileName);
+    qDebug()<<QString("     This tool is used to convert pro files into pri files.");
+}
+
+void loadDataToProFileReader(const QString &filPath, ProFileReader &proFileReader)
+{
+    QFile file(filPath);
+    if(!file.open(QIODevice::ReadOnly)) {
+        qDebug()<<"error: load sources file failed";
+        return;
+    }
+    while(!file.atEnd()) {
+        proFileReader.loadOneRowOfData(QString(file.readLine()));
+    }
+    file.close();
+}
+
+void convertedToPri(const QString &dstPath, const QStringList &configList)
+{
+    ParseProConfig parseProConfig;
+
+    QFile output(dstPath);
+    if(!output.open(QIODevice::WriteOnly)) {
+        qDebug()<<"error: create destination file failed";
+        return;
+    }
+
+    for(int i=0;i<configList.length();i++) {
+        QString convertConfig = parseProConfig.convertOneConfig(configList.at(i));
+        QByteArray dataLine = convertConfig.toLatin1();
+        output.write(dataLine);
+    }
+    output.close();
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QFile file("/home/chen/Documents/qtProject/ConvertProToPri/ConvertProToPriTest/rayware.pro");
-    if(!file.open(QIODevice::ReadOnly))
+    if(argc != 2) {
+        QFileInfo fileInfo(argv[0]);
+        printUsage(fileInfo.fileName());
         return -1;
+    }
 
     ProFileReader proFileReader;
-    while(!file.atEnd()) {
-        proFileReader.loadOneRowOfData(QString(file.readLine()));
-    }
-    file.close();
-
-    QStringList configList = proFileReader.splitConfigFromData();
-
-    QFile waitForConverting("./waitConverted");
-    if(waitForConverting.open(QIODevice::WriteOnly)) {
-        for(int i=0;i<configList.length();i++) {
-            waitForConverting.write(configList.at(i).toLatin1());
-        }
-        waitForConverting.close();
-    }
-
-    ParseProConfig parseProConfig;
-
-    QFile output("./output");
-    if(!output.open(QIODevice::WriteOnly)) {
-        return -1;
-    }
-
-    qDebug()<<"===========================================";
-    for(int i=0;i<configList.length();i++) {
-        qDebug()<<"===========================================";
-        qDebug()<<"config.at("<<i<<"):"<<configList.at(i);
-        qDebug()<<"===========================================";
-        QString convertConfig = parseProConfig.convertOneConfig(configList.at(i));
-        qDebug()<<"===========================================";
-        qDebug()<<"convertConfig"<<i<<":"<<convertConfig;
-        QByteArray dataLine = convertConfig.toLatin1();
-        output.write(dataLine);
-    }
-    output.close();
+    loadDataToProFileReader(argv[1], proFileReader);
+    convertedToPri(argv[1], proFileReader.splitConfigFromData());
 
     return a.exec();
 }
